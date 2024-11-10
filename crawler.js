17ea8cdbe776460b82b538520f3e257da732a587
Hsing <skylinegtr0224@gmail.com>
2024-11-10 09:34:05 +0800

    調整 message 斷行判斷

--- crawler.js
+++ crawler.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const axios = require('axios');
 const itemArray = {
-	energy: "能量",
-	mana: "魔力石",
-	crystal: "水晶",
-	summon_exclusive: "刻印召喚書碎片",
-	scroll_water: "水屬性召喚書",
-	scroll_fire: "火屬性召喚書",
-	scroll_wind: "風屬性召喚書",
-	scroll_mystical: "神秘召喚書",
-	scroll_light_and_dark: "光明/黑暗召喚書",
+  energy: "能量",
+  mana: "魔力石",
+  crystal: "水晶",
+  summon_exclusive: "刻印召喚書碎片",
+  scroll_water: "水屬性召喚書",
+  scroll_fire: "火屬性召喚書",
+  scroll_wind: "風屬性召喚書",
+  scroll_mystical: "神秘召喚書",
+  scroll_light_and_dark: "光明/黑暗召喚書",
 }
 
 /**
@@ -19,17 +19,17 @@ const itemArray = {
  *                           如果發生錯誤或數據無效,則回傳空陣列。
  */
 function getRecordData() {
-	return new Promise((resolve, reject) => {
-		fs.readFile('record.json', 'utf8', (err, data) => {
-			if (err) {
-				console.error('Error reading JSON file:', err);
-				resolve([]);
-			} else {
-				const responseData = data && JSON.parse(data) || [];
-				resolve(responseData);
-			}
-		});
-	});
+  return new Promise((resolve, reject) => {
+    fs.readFile('record.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading JSON file:', err);
+        resolve([]);
+      } else {
+        const responseData = data && JSON.parse(data) || [];
+        resolve(responseData);
+      }
+    });
+  });
 }
 
 /**
@@ -38,16 +38,16 @@ function getRecordData() {
  * @param {*} data
  */
 function writeRecord(data) {
-	const jsonData = JSON.stringify(data, null, 2);
+  const jsonData = JSON.stringify(data, null, 2);
 
-	// 寫入 JSON 檔案
-	fs.writeFile('record.json', jsonData, (err) => {
-		if (err) {
-			console.error('Error writing JSON file:', err);
-		} else {
-			console.log('Data has been written to recode.json');
-		}
-	});
+  // 寫入 JSON 檔案
+  fs.writeFile('record.json', jsonData, (err) => {
+    if (err) {
+      console.error('Error writing JSON file:', err);
+    } else {
+      console.log('Data has been written to recode.json');
+    }
+  });
 }
 
 /**
@@ -76,57 +76,57 @@ function writeRecord(data) {
 * @returns {Array<CrawlerItem>} The filtered and transformed data
 */
 function getCrawlerData(data) {
-	return data
-		.filter(x => x.Status === 'verified' && Number(x.Score) >= 0)
-		.map(({ Label, Resources }) => {
-			return {
-				Label,
-				Resource: Resources.map(resource => ({
-					Quantity: resource.Quantity,
-					Code: resource.Sw_Resource.Code
-				}))
-			};
-		});
+  return data
+    .filter(x => x.Status === 'verified' && Number(x.Score) >= 0)
+    .map(({ Label, Resources }) => {
+      return {
+        Label,
+        Resource: Resources.map(resource => ({
+          Quantity: resource.Quantity,
+          Code: resource.Sw_Resource.Code
+        }))
+      };
+    });
 }
 
 // 抓取資料並作動
 axios.get('https://swq.jp/_special/rest/Sw/Coupon')
-	.then(async response => {
-		const data = response.data?.data;
-		if (data) {
-			// 取得上一次紀錄的最後一筆資料
-			const recordData = await getRecordData();
+  .then(async response => {
+    const data = response.data?.data;
+    if (data) {
+      // 取得上一次紀錄的最後一筆資料
+      const recordData = await getRecordData();
 
-			// 將抓取的資料做整理
-			const result = getCrawlerData(data);
+      // 將抓取的資料做整理
+      const result = getCrawlerData(data);
 
-			const lastDataIndex = recordData.length
-				? result.findIndex(item => item.Label === recordData[0].Label)
-				: -1;
+      const lastDataIndex = recordData.length
+        ? result.findIndex(item => item.Label === recordData[0].Label)
+        : -1;
 
-			const uniqueData = lastDataIndex === -1
-				? result
-				: result.slice(0, lastDataIndex);
+      const uniqueData = lastDataIndex === -1
+        ? result
+        : result.slice(0, lastDataIndex);
 
-			if (uniqueData.length) {
-				// 寫入整合資料的第一筆作為下次索引用
-				writeRecord([uniqueData[0]]);
+      if (uniqueData.length) {
+        // 寫入整合資料的第一筆作為下次索引用
+        writeRecord([uniqueData[0]]);
 
-				const outputLinkArray = uniqueData.map((x) => {
-					const link = `http://withhive.me/313/${x.Label}`;
-					const items = x.Resource.map((item) => `${itemArray[item.Code] ?? item.Code}x${item.Quantity}`);
-					return `${link}:${items.join('、')}`;
-				});
-				console.log('UNIQUE_DATA:' + JSON.stringify(outputLinkArray))
+        const outputLinkArray = uniqueData.map((x) => {
+          const link = `http://withhive.me/313/${x.Label}`;
+          const items = x.Resource.map((item) => `${itemArray[item.Code] ?? item.Code}x${item.Quantity}`);
+          return `${link}：${items.join('、')}`;
+        });
+        console.log('UNIQUE_DATA:' + JSON.stringify(outputLinkArray))
 
-				// 輸出到控制台，GitHub Actions 可以捕獲這個輸出
-				console.log(output);
-			} else {
-				console.log('UNIQUE_DATA:[]');
-			}
+        // 輸出到控制台，GitHub Actions 可以捕獲這個輸出
+        console.log(output);
+      } else {
+        console.log('UNIQUE_DATA:[]');
+      }
 
-		}
-	})
-	.catch(error => {
-		console.error('Error fetching JSON data:', error);
-	});
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching JSON data:', error);
+  });
